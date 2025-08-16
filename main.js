045027538a8c6c8922e9a82e056e756c282600be
laurinha-siebert <144138045+laurinha-siebert@users.noreply.github.com>
2025-08-16 20:29:15 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,36 +1,48 @@
-function classificaSenha() {
-    let entropia = tamanhoSenha * Math.log2(alfabeto.length);
-    console.log(entropia);
-    function geraSenha() {
-    let alfabeto = '';
-    // código omitido
-    classificaSenha(alfabeto.length);
-}function classificaSenha(tamanhoAlfabeto) {
-    // código omitido
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+
+function diminuiTamanho(){ 
+    tamanhoSenha = tamanhoSenha - 1;
 }
-if (entropia > 11){
-    forcaSenha.classList.add('forte');
-} else if (entropia > 5 && entropia < 12 ) {
-    forcaSenha.classList.add('media');
-} else if (entropia <= 5){
-    forcaSenha.classList.add('fraca');
-}if (entropia > 57){
-    forcaSenha.classList.add('forte');
-} else if (entropia > 35 && entropia < 57 ) {
-    forcaSenha.classList.add('media');
-} else if (entropia <= 35){
-    forcaSenha.classList.add('fraca');
-}const valorEntropia = document.querySelector('.entropia');
-valorEntropia.textContent = entropia;
-valorEntropia.textContent = 2**Math.floor(entropia);valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
 
-
-
-
-valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
-valorEntropia.textContent = Math.floor(2**entropia/(100e6*60*60*24))
-
-valorEntropia.textContent = Math.floor(2**entropia/(100e6*60*60*24));
-valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24));
-valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
-
+console.log(botoes);function diminuiTamanho(){ 
+    tamanhoSenha = tamanhoSenha - 1;
+    numeroSenha.textContent = tamanhoSenha
+}
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+function aumentaTamanho() {
+    tamanhoSenha = tamanhoSenha + 1;
+    numeroSenha.textContent = tamanhoSenha;
+}function diminuiTamanho() {
+    if (tamanhoSenha > 1)
+    tamanhoSenha = tamanhoSenha - 1;
+    numeroSenha.textContent = tamanhoSenha;
+}function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
+        tamanhoSenha = tamanhoSenha - 1;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
+            tamanhoSenha = tamanhoSenha + 1;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
+        // tamanhoSenha = tamanhoSenha - 1;
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
+            // tamanhoSenha = tamanhoSenha + 1;
+            tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
