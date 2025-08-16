50372a8e181bcc55f1fc607c3f234f99744093b4
laurinha-siebert <144138045+laurinha-siebert@users.noreply.github.com>
2025-08-16 20:14:29 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
+valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) 
+  + " dias para descobrir essa senha.";
 valorEntropia.textContent = Math.floor(2**entropia/(100e6*60*60*24))
-
 valorEntropia.textContent = Math.floor(2**entropia/(100e6*60*60*24));
 valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24));
-valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
+valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) 
+  + " dias para descobrir essa senha.";
